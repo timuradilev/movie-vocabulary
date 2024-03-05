543513bec47e1d22248c0444e7935563605e3f64
atimur <atimur@zoon.ru>
2024-03-05 22:05:02 +0100

    feat: dummy api request

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css'
 import WordList from './WordList.jsx'
+import api from './api/words'
+import React, {useState, useEffect} from 'react';
 
 function App() {
-    const words = [
-        {id: 'word1', left: 'left txt dasdsadsadas', middle: 'word1', right: 'right txt'},
-        {id: 'word2', left: 'left txt', middle: 'wordfdsfsdd2', right: 'right txtdsfsdfd'},
-        {id: 'word3', left: 'left txt', middle: 'word3', right: 'right txt'},
-        {id: 'word4', left: 'ldsffdsfsdfdseft txt', middle: 'word4', right: 'right txtdsfds'},
-        {id: 'word5', left: 'left txt', middle: 'word5', right: 'right txt'}
-    ];
+    const [category, setCategory] = useState('');
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        const fetchWords = async () => {
+            try {
+                const response = await api.get('/words.json');
+                setCategory(response.data.category);
+                setWords(response.data.words);
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        }
+        
+        fetchWords();
+    }, [])
     return (
-        <WordList category="A1" items={words}/>
+        <WordList category={category} items={words}/>
     )
 }
 
