ea4d4f087d8a1862132b045287c92ce3326712f9
atimur <atimur@zoon.ru>
2024-03-21 23:02:01 +0100

    feat: improved the sanitizer.

--- backend/app/src/Extractor/Filters/SanitizeFilter.php
+++ backend/app/src/Extractor/Filters/SanitizeFilter.php
@@ -7,7 +7,36 @@ use App\Extractor\FilterInterface;
 final class SanitizeFilter implements FilterInterface {
 
 	public function filter(string $subtitleLine): ?string {
-		return preg_replace(['/\n/', '/\.\.\./', '/’/'], [' ', '', '\''], $subtitleLine);
+		$subtitleLine = preg_replace(
+			[
+				'/\n/',
+				'/\.\.\./',
+				'/’/',
+			],
+			[
+				' ',
+				'',
+				'\'',
+			],
+			$subtitleLine,
+		);
+
+		$subtitleLine = preg_replace(
+			[
+				'/(in\')(\W|$)/iu',
+				'/(^|\s)[^a-z]+([a-z])/i',
+				'/([a-z])[^a-z]+(\s|$)/i',
+			],
+			[
+				'ing$2',
+				'$1$2',
+				'$1$2',
+			],
+			$subtitleLine,
+		);
+		// todo: cousin's => cousin
+
+		return strtolower($subtitleLine);
 	}
 
 }
